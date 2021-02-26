6b3221037260628482779da666adf6049f365533
Ryan <jesselockard99@gmail.com>
2021-02-25 22:56:16 -0500

    added mock dropdown and fluid prop to DashLayout

--- web/src/layouts/DashLayout/DashLayout.tsx
+++ web/src/layouts/DashLayout/DashLayout.tsx
@@ -1,13 +1,27 @@
+import { useMemo } from 'react'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Dropdown from 'react-bootstrap/Dropdown'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import { Link, routes } from '@redwoodjs/router'
+import { BsPeopleCircle } from 'react-icons/bs'
+import { Link, routes, useMatch } from '@redwoodjs/router'
 
 import NavLink from 'src/components/common/NavLink'
 
 import './DashLayout.css'
 
-const DashLayout = ({ children }) => {
-  console.log(routes)
+/**
+ * Primary `Layout` for the Palletized application.
+ *
+ * Accepted Props:
+ *
+ * * `fluid` - can be used to remove margins and apply the `fluid` prop to the
+ * underlying Bootstrap `<Container>` wrapping `children` content.
+ */
+const DashLayout = ({ children, fluid }) => {
+  const warehousesRoute = useMemo(() => routes.warehouses(), [])
+  const warehousesMatch = useMatch(warehousesRoute).match
 
   return (
     <>
@@ -22,14 +36,74 @@ const DashLayout = ({ children }) => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse>
-          <Nav>
+          <Nav className="mr-auto">
             <NavLink linkTo={routes.dashboard()}>Dashboard</NavLink>
+            <Dropdown>
+              <Dropdown.Toggle
+                as={Nav.Link}
+                className={warehousesMatch && 'active'}
+              >
+                Warehouses
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  className={warehousesMatch && 'active'}
+                  href={warehousesRoute}
+                >
+                  Warehouses
+                </Dropdown.Item>
+                <Dropdown.Item>Locations</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <NavLink linkTo={routes.locations()}>Locations</NavLink>
+            <NavLink linkTo={routes.orders()}>Orders</NavLink>
+            <NavLink linkTo={routes.pallets()}>Pallets</NavLink>
+          </Nav>
+          <Nav>
+            <Dropdown as={Nav.Item}>
+              <Dropdown.Toggle id="nav-actions-dropdown" as={Nav.Item}>
+                <Button
+                  className="nav-actions-button d-flex flex-row align-items-center text-decoration-none"
+                  variant="link"
+                >
+                  <span className="d-flex flex-column">
+                    <span>Ryan Lockard</span>
+                  </span>
+                  <BsPeopleCircle className="ml-4" size={16} />
+                </Button>
+              </Dropdown.Toggle>
+              <Dropdown.Menu align="right">
+                {/*  */}
+                <Dropdown.Header>Configuration</Dropdown.Header>
+                <Dropdown.Item>Account Settings</Dropdown.Item>
+                <Dropdown.Item>Warehouse Settings</Dropdown.Item>
+                {/*  */}
+                <Dropdown.Header>Support</Dropdown.Header>
+                <Dropdown.Item>User Manual</Dropdown.Item>
+                <Dropdown.Item>Support Center</Dropdown.Item>
+                {/*  */}
+                <Dropdown.Header>Management</Dropdown.Header>
+                <Dropdown.Item className="text-primary switch-warehouse">
+                  Switch Warehouse
+                </Dropdown.Item>
+                <Dropdown.Item className="text-danger logout">
+                  Logout
+                </Dropdown.Item>
+                {/*  */}
+              </Dropdown.Menu>
+            </Dropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      {children}
+      <Container className={!fluid && 'my-4'} fluid={fluid}>
+        {children}
+      </Container>
     </>
   )
+}
+
+DashLayout.defaultProps = {
+  fluid: false,
 }
 
 export default DashLayout
