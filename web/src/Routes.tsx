da255d94b1e3e6ff00f57c695f451b835911926a
Ryan <jesselockard99@gmail.com>
2021-02-25 22:03:50 -0500

    converted Route.js->Route.tsx

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -14,13 +14,33 @@ const Routes = () => {
     <Router>
       <Route path="/" page={DashboardPage} name="dashboard" />
       {/* Pallet-Products */}
-      <Route path="/pallet-products/new" page={NewPalletProductPage} name="newPalletProduct" />
-      <Route path="/pallet-products/{id}/edit" page={EditPalletProductPage} name="editPalletProduct" />
-      <Route path="/pallet-products/{id}" page={PalletProductPage} name="palletProduct" />
-      <Route path="/pallet-products" page={PalletProductsPage} name="palletProducts" />
+      <Route
+        path="/pallet-products/new"
+        page={NewPalletProductPage}
+        name="newPalletProduct"
+      />
+      <Route
+        path="/pallet-products/{id}/edit"
+        page={EditPalletProductPage}
+        name="editPalletProduct"
+      />
+      <Route
+        path="/pallet-products/{id}"
+        page={PalletProductPage}
+        name="palletProduct"
+      />
+      <Route
+        path="/pallet-products"
+        page={PalletProductsPage}
+        name="palletProducts"
+      />
       {/* Pallets */}
       <Route path="/pallets/new" page={NewPalletPage} name="newPallet" />
-      <Route path="/pallets/{id}/edit" page={EditPalletPage} name="editPallet" />
+      <Route
+        path="/pallets/{id}/edit"
+        page={EditPalletPage}
+        name="editPallet"
+      />
       <Route path="/pallets/{id}" page={PalletPage} name="pallet" />
       <Route path="/pallets" page={PalletsPage} name="pallets" />
       {/* Orders */}
@@ -30,21 +50,37 @@ const Routes = () => {
       <Route path="/orders" page={OrdersPage} name="orders" />
       {/* Products */}
       <Route path="/products/new" page={NewProductPage} name="newProduct" />
-      <Route path="/products/{id}/edit" page={EditProductPage} name="editProduct" />
+      <Route
+        path="/products/{id}/edit"
+        page={EditProductPage}
+        name="editProduct"
+      />
       <Route path="/products/{id}" page={ProductPage} name="product" />
       <Route path="/products" page={ProductsPage} name="products" />
       {/* Locations */}
       <Route path="/locations/new" page={NewLocationPage} name="newLocation" />
-      <Route path="/locations/{id}/edit" page={EditLocationPage} name="editLocation" />
+      <Route
+        path="/locations/{id}/edit"
+        page={EditLocationPage}
+        name="editLocation"
+      />
       <Route path="/locations/{id}" page={LocationPage} name="location" />
       <Route path="/locations" page={LocationsPage} name="locations" />
       {/* Warehouses */}
-      <Route path="/warehouses/new" page={NewWarehousePage} name="newWarehouse" />
-      <Route path="/warehouses/{id}/edit" page={EditWarehousePage} name="editWarehouse" />
+      <Route
+        path="/warehouses/new"
+        page={NewWarehousePage}
+        name="newWarehouse"
+      />
+      <Route
+        path="/warehouses/{id}/edit"
+        page={EditWarehousePage}
+        name="editWarehouse"
+      />
       <Route path="/warehouses/{id}" page={WarehousePage} name="warehouse" />
       <Route path="/warehouses" page={WarehousesPage} name="warehouses" />
       {/* Misc */}
-      <Route notfound page={NotFoundPage} />
+      <Route notfound={NotFoundPage} page={NotFoundPage} />
     </Router>
   )
 }
