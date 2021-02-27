a36fc3d1d5e5d5c1c3892d8a77f9e6d654b0f0d2
Ryan <jesselockard99@gmail.com>
2021-02-27 18:34:45 -0500

    converted DashboardPage to TypeScript

--- web/src/pages/DashboardPage/DashboardPage.tsx
+++ web/src/pages/DashboardPage/DashboardPage.tsx
@@ -5,14 +5,8 @@ import DashLayout from 'src/layouts/DashLayout/DashLayout'
 const DashboardPage = () => {
   return (
     <DashLayout>
-      <h1>DashboardPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/DashboardPage/DashboardPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>dashboard</code>, link to me with `
-        <Link to={routes.dashboard()}>Dashboard</Link>`
-      </p>
+      <h1>Dashboard</h1>
+      <p className="text-muted">An overview of ACME Chaleston.</p>
     </DashLayout>
   )
 }
