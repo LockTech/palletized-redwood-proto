3c1c5f23019becb27ddc45802b2b94304845848a
Ryan <jesselockard99@gmail.com>
2021-02-28 15:46:31 -0500

    added DashTileCard component to DashboardPage

---
+++ web/src/pages/DashboardPage/components/DashTileCard/DashTileCard.tsx
@@ -0,0 +1,37 @@
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import { BsInfoCircle } from 'react-icons/bs'
+
+import TileCard from 'src/components/TileCard/TileCard'
+
+export interface DashTileCardProps {
+  icon: (size: number) => React.ReactNode
+  text: React.ReactNode
+  title: string
+  toolTip: (props: unknown) => React.ReactNode
+}
+
+const DashTileCard: React.FC<DashTileCardProps> = ({
+  icon,
+  text,
+  title,
+  toolTip,
+}) => (
+  <TileCard
+    header={
+      <div className="d-flex flex-direction-row align-items-center">
+        <p className="mb-0">{title}</p>
+        <OverlayTrigger
+          placement="bottom-start"
+          delay={{ show: 30, hide: 1240 }}
+          overlay={toolTip}
+        >
+          <BsInfoCircle className="ml-2" />
+        </OverlayTrigger>
+      </div>
+    }
+    icon={icon}
+    text={<p className="display-4 mb-0 text-right">{text}</p>}
+  />
+)
+
+export default DashTileCard
