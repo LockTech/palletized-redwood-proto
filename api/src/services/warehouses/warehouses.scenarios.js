4a6f5aeb9080fa787c31958fa67048a86dda1842
Ryan <jesselockard99@gmail.com>
2021-02-28 00:42:33 -0500

    added defineScenario import

--- api/src/services/warehouses/warehouses.scenarios.js
+++ api/src/services/warehouses/warehouses.scenarios.js
@@ -1,3 +1,5 @@
+import { defineScenario } from '@redwoodjs/testing/dist/scenario'
+
 export const standard = defineScenario({
   warehouse: {
     one: { name: 'String6393517', updatedAt: '2021-02-26T18:52:47Z' },
