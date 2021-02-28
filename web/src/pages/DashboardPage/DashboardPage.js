8615eb1876653579338b4dfc7aacbfd8c2927c91
Ryan <jesselockard99@gmail.com>
2021-02-28 04:07:56 -0500

    DashboardPage TS->JS

--- web/src/pages/DashboardPage/DashboardPage.js
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -1,12 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
-
 import DashLayout from 'src/layouts/DashLayout/DashLayout'
+import ActiveOrderCountCardCell from 'src/components/ActiveOrderCountCardCell'
 
 const DashboardPage = () => {
   return (
     <DashLayout>
       <h1>Dashboard</h1>
       <p className="text-muted">An overview of ACME Chaleston.</p>
+      <ActiveOrderCountCardCell warehouseId="064b12ba-468d-4c29-b852-b1a5ced654c0" />
     </DashLayout>
   )
 }
