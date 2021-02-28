a6f309285468d6cbf5391136026b38cb1b77e28c
Ryan <jesselockard99@gmail.com>
2021-02-28 16:27:08 -0500

    impl Header and ActiveCardCountCell on DashboardPage

--- web/src/pages/DashboardPage/DashboardPage.js
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -1,12 +1,44 @@
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Tooltip from 'react-bootstrap/Tooltip'
+import { BsArchive } from 'react-icons/bs'
+
 import DashLayout from 'src/layouts/DashLayout/DashLayout'
-import ActiveOrderCountCardCell from 'src/components/ActiveOrderCountCardCell'
+import ActiveOrderCountCell from 'src/components/ActiveOrderCountCell'
+
+import DashTileCard from './components/DashTileCard/DashTileCard'
+
+const renderActiveOrderTooltip = (props) => (
+  <Tooltip id="header-tooltip" {...props}>
+    The number of active <strong>Orders</strong> which have{' '}
+    <strong>Pallets</strong> located at this warehouse.
+  </Tooltip>
+)
 
 const DashboardPage = () => {
   return (
     <DashLayout>
-      <h1>Dashboard</h1>
-      <p className="text-muted">An overview of ACME Chaleston.</p>
-      <ActiveOrderCountCardCell warehouseId="064b12ba-468d-4c29-b852-b1a5ced654c0" />
+      <Row>
+        <Col>
+          <h1>Dashboard</h1>
+          <p className="text-muted">
+            Get an overview of the <strong>Jorbs</strong>,{' '}
+            <strong>Orders</strong>, and <strong>Pallets</strong> of Charleston.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <DashTileCard
+            icon={(size) => <BsArchive className="text-primary" size={size} />}
+            text={
+              <ActiveOrderCountCell warehouseId="064b12ba-468d-4c29-b852-b1a5ced654c0" />
+            }
+            title={'No. of Active Orders'}
+            toolTip={renderActiveOrderTooltip}
+          />
+        </Col>
+      </Row>
     </DashLayout>
   )
 }
