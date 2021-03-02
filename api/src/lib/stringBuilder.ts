1554c4a6fe18dc20f158b17fae3267c83fa06035
Ryan <jesselockard99@gmail.com>
2021-03-02 16:27:48 -0500

    added global-flag to reservedCharRegEx

--- api/src/lib/stringBuilder.ts
+++ api/src/lib/stringBuilder.ts
@@ -1,6 +1,6 @@
 // https://pretty-rfc.herokuapp.com/RFC3986#reserved
 // Includes all "reserved" characters (gen-delims / sub-delims)
 // With '.' added to let values remain NATS-safe (future-profing)
-export const reservedCharRegEx = /([:\/?#\[\]@!$&'()*+,;="<>%{}|\\^`.])+/
+export const reservedCharRegEx = /([:\/?#\[\]@!$&'()*+,;="<>%{}|\\^`.])+/g
 
-export const wsRegEx = /(\ )+/
+// export const wsRegEx = /(\ )+/g
