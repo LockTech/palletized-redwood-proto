25701a1cdf6f80f9af03719bdfcc477de6404ab0
Ryan <jesselockard99@gmail.com>
2021-03-04 17:55:11 -0500

    updated Warehouse List Page

--- web/src/pages/WHListPage/WHListPage.js
+++ web/src/pages/WHListPage/WHListPage.js
@@ -1,10 +1,43 @@
+import { Link, routes } from '@redwoodjs/router'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
 import DashLayout from 'src/layouts/DashLayout/DashLayout'
 import WarehousesCell from 'src/components/WarehousesCell'
 
 const WHListPage = () => {
   return (
     <DashLayout>
-      <WarehousesCell />
+      <Container className="palletized-container">
+        <Row>
+          <Col>
+            <h1>Warehouses</h1>
+            <p className="text-muted">
+              Geographically distinct areas for storing Pallets.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              as={Link}
+              block
+              className="mb-3"
+              to={routes.newWarehouse()}
+              variant="primary"
+            >
+              Create New Warehouse
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <WarehousesCell />
+          </Col>
+        </Row>
+      </Container>
     </DashLayout>
   )
 }
