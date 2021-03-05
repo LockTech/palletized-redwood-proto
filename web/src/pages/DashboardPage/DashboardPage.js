37e274015996fe0fdd7d0137fac9646fe44f4484
Ryan <jesselockard99@gmail.com>
2021-03-05 15:55:21 -0500

    updated use of ActiveOrderCountCell on DashboardPage

--- web/src/pages/DashboardPage/DashboardPage.js
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -1,26 +1,10 @@
-import { routes } from '@redwoodjs/router'
 import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Tooltip from 'react-bootstrap/Tooltip'
-import { BsArchive, BsClock } from 'react-icons/bs'
 
 import DashLayout from 'src/layouts/DashLayout/DashLayout'
 import ActiveOrderCountCell from 'src/components/ActiveOrderCountCell'
 import UpcomingOrderCountCell from 'src/components/UpcomingOrderCountCell'
-
-import DashTileCard from './components/DashTileCard/DashTileCard'
-import Container from 'react-bootstrap/esm/Container'
-
-const renderActiveOrderTooltip = (props) => (
-  <Tooltip id="header-tooltip" {...props}>
-    The number of open Orders which have Pallets located in this warehouse.
-  </Tooltip>
-)
-const renderUpcomingOrderTooltip = (props) => (
-  <Tooltip id="header-tooltip" {...props}>
-    The number of Orders with ship-dates within the next seven (7) days.
-  </Tooltip>
-)
 
 const DashboardPage = () => {
   return (
@@ -37,34 +21,10 @@ const DashboardPage = () => {
         </Row>
         <Row>
           <Col sm={12} md={6} className="mb-3 mb-md-0">
-            <DashTileCard
-              footer={{
-                text: 'Active Orders',
-                to: routes.warehouses(),
-              }}
-              icon={(size) => (
-                <BsArchive className="text-primary" size={size} />
-              )}
-              header={'Active Orders'}
-              headerTooltip={renderActiveOrderTooltip}
-            >
-              <ActiveOrderCountCell warehouseId="064b12ba-468d-4c29-b852-b1a5ced654c0" />
-            </DashTileCard>
+            <ActiveOrderCountCell warehouseId="064b12ba-468d-4c29-b852-b1a5ced654c0" />
           </Col>
           <Col sm={12} md={6} className="mb-3 mb-md-0">
-            <DashTileCard
-              footer={{
-                text: 'Upcoming Deliveries',
-                to: routes.warehouses(),
-              }}
-              icon={(size) => (
-                <BsClock className="text-secondary" size={size} />
-              )}
-              header={'Upcoming Deliveries'}
-              headerTooltip={renderUpcomingOrderTooltip}
-            >
-              <UpcomingOrderCountCell warehouseId="064b12ba-468d-4c29-b852-b1a5ced654c0" />
-            </DashTileCard>
+            <UpcomingOrderCountCell warehouseId="064b12ba-468d-4c29-b852-b1a5ced654c0" />
           </Col>
         </Row>
       </Container>
