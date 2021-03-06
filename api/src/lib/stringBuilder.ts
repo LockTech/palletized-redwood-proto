b5a7fd5b8bb67f206532c4a1adc393fe4a94d9a4
Ryan <jesselockard99@gmail.com>
2021-03-06 17:39:10 -0500

    removed period from reserved character regex

--- api/src/lib/stringBuilder.ts
+++ api/src/lib/stringBuilder.ts
@@ -1,6 +1,5 @@
 // https://pretty-rfc.herokuapp.com/RFC3986#reserved
 // Includes all "reserved" characters (gen-delims / sub-delims)
-// With '.' added to let values remain NATS-safe (future-profing)
-export const reservedCharRegEx = /([:\/?#\[\]@!$&'()*+,;="<>%{}|\\^`.])+/g
+export const reservedCharRegEx = /([:\/?#\[\]@!$&'()*+,;="<>%{}|\\^`])+/g
 
 // export const wsRegEx = /(\ )+/g
