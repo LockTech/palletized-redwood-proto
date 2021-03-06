7dd79eb43366241175e9e3e423a1d6b3c48fabb6
Ryan <jesselockard99@gmail.com>
2021-03-05 19:27:18 -0500

    updated UpcomingOrderCountCell's TileCard use

--- web/src/components/UpcomingOrderCountCell/UpcomingOrderCountCell.js
+++ web/src/components/UpcomingOrderCountCell/UpcomingOrderCountCell.js
@@ -1,19 +1,38 @@
+import { routes } from '@redwoodjs/router'
 import Skeleton from 'react-loading-skeleton'
+
+import TileCard from 'src/components/TileCard'
 
 export const QUERY = gql`
   query UpcomingOrderCountQuery($id: String!) {
     count: orderCountInWarehouse(warehouseId: $id)
-    # Pending check for order.deliveryDate
   }
 `
 
-export const Loading = () => <Skeleton />
+const CommonTileCard = ({ children }) => (
+  <TileCard
+    footer={{
+      text: 'Upcoming Deliveries',
+      to: routes.warehouses(),
+    }}
+    header="Upcoming Deliveries"
+    headerTooltip="The number of Active Orders at this Warehouse which have delivery-dates within the next seven (7) days."
+  >
+    <p className="mb-0 display-4">{children}</p>
+  </TileCard>
+)
+
+export const Loading = () => (
+  <CommonTileCard>
+    <Skeleton />
+  </CommonTileCard>
+)
 
 export const Empty = () => '0'
 
 export const Failure = ({ _error }) => {
   // perform error notification
-  return '-'
+  return <CommonTileCard>-</CommonTileCard>
 }
 
-export const Success = ({ count }) => count || '0'
+export const Success = ({ count }) => <CommonTileCard>{count}</CommonTileCard>
