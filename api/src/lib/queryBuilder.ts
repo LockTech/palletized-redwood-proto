309f28191abbb58a474ee86f75409a4513957d74
Ryan <jesselockard99@gmail.com>
2021-03-06 17:39:22 -0500

    added documentation for eqQueryFromOpts

--- api/src/lib/queryBuilder.ts
+++ api/src/lib/queryBuilder.ts
@@ -1,3 +1,13 @@
+/**
+ * Converts `options` into a Prisma [equal filter-query](https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#equals)
+ * to be used as part of a high-level [model query option](https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#model-query-options).
+ *
+ * **Note:** The use of this function is not nessicary as per Prisma's documentation on equal filters.
+ * This function serves as an example for queryies which *require* additional formatting.
+ *
+ * @param options Options to extract query from.
+ * @returns A Prisma equal-query for all `options`
+ */
 export const eqQueryFromOpts = (options: unknown) => {
   if (!options) return null
 
