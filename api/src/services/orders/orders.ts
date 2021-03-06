b017b6e1faba82b4be550212b9c07d297d8f74f3
Ryan <jesselockard99@gmail.com>
2021-03-06 17:40:48 -0500

    altered orders service, impl createOrder

--- api/src/services/orders/orders.ts
+++ api/src/services/orders/orders.ts
@@ -1,20 +1,52 @@
+import { UserInputError } from '@redwoodjs/api'
 import { db } from 'src/lib/db'
-import { eqQueryFromOpts } from 'src/lib/queryBuilder'
+import { throwIfIsReserved, throwIfTooLong } from 'src/lib/error'
 
+const MAX_ORDER_NUMBER_LEN = 75
+const MAX_JOB_NAME_LEN = 75
+
+// ==
 export const orders = () => {
   return db.order.findMany()
 }
+//
 
+// ==
 export const order = ({ id }) => {
   return db.order.findUnique({ where: { id } })
 }
+//
 
+// ==
+export const createOrder = async ({ input }: { input: IOrder }) => {
+  throwIfIsReserved(input.orderNumber, 'Order', 'Number')
+  throwIfTooLong(input.orderNumber, MAX_ORDER_NUMBER_LEN, 'Order', 'Number')
+
+  if (input.jobName !== undefined) {
+    throwIfTooLong(input.jobName, MAX_JOB_NAME_LEN, 'Order', 'Job Name')
+  }
+
+  input.id = input.orderNumber.replaceAll(' ', '-').toLowerCase()
+
+  try {
+    return await db.order.create({
+      data: input,
+    })
+  } catch (err) {
+    throw new UserInputError('test', {
+      messages: {
+        Error: [err.code],
+      },
+    })
+  }
+}
+//
+
+// ==
 export const orderCountInWarehouse = ({ warehouseId, order }) => {
-  const query = eqQueryFromOpts(order)
-
   return db.order.count({
     where: {
-      ...query,
+      ...order,
       pallets: {
         some: {
           location: {
@@ -27,8 +59,11 @@ export const orderCountInWarehouse = ({ warehouseId, order }) => {
     },
   })
 }
+//
 
+// ==
 export const Order = {
   pallets: (_obj, { root }) =>
     db.order.findUnique({ where: { id: root.id } }).pallets(),
 }
+//
