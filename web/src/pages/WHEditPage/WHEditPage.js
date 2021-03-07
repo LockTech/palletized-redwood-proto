bdb5b5a910f0c6432a16623bed020b884d7930bc
Ryan <jesselockard99@gmail.com>
2021-03-07 03:36:02 -0500

    added edit-Alert to WHEditPage

--- web/src/pages/WHEditPage/WHEditPage.js
+++ web/src/pages/WHEditPage/WHEditPage.js
@@ -1,9 +1,11 @@
+import Alert from 'react-bootstrap/Alert'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
 import DashLayout from 'src/layouts/DashLayout/DashLayout'
 import WarehouseEditCell from 'src/components/WarehouseEditCell'
+import WarehouseNameCell from 'src/components/WarehouseNameCell'
 
 const WHEditPage = ({ id }) => {
   return (
@@ -15,6 +17,17 @@ const WHEditPage = ({ id }) => {
             <p className="text-muted">
               Alter the details of a Warehouse managed by your organization.
             </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Alert variant="info">
+              You are currently editing the{' '}
+              <strong>
+                <WarehouseNameCell id={id} />
+              </strong>{' '}
+              Warehouse.
+            </Alert>
           </Col>
         </Row>
         <Row>
