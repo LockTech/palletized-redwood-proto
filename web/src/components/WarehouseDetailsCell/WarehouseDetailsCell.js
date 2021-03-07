a3738e23b90011f740ca91ee2808bd3f519e48b3
Ryan <jesselockard99@gmail.com>
2021-03-07 16:44:48 -0500

    added Empty WarehouseDetails error handling

--- web/src/components/WarehouseDetailsCell/WarehouseDetailsCell.js
+++ web/src/components/WarehouseDetailsCell/WarehouseDetailsCell.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useFlash } from '@redwoodjs/web'
 import Card from 'react-bootstrap/Card'
 
 import LoadingCard from 'src/components/LoadingCard'
@@ -16,7 +18,30 @@ export const QUERY = gql`
 
 export const Loading = () => <LoadingCard header="Warehouse Details" />
 
-export const Empty = () => <div>Warehouse not found</div>
+export const Empty = ({ id }) => {
+  const { addMessage } = useFlash()
+
+  useEffect(() => {
+    addMessage(
+      `Could not find details for Warehouse: ${id}. Are you sure the Warehouse exist?`,
+      {
+        skipTimeout: true,
+        variant: 'danger',
+      }
+    )
+  }, [addMessage, id])
+
+  return (
+    <>
+      <Card>
+        <Card.Header>Details</Card.Header>
+        <Card.Body>
+          <Card.Text>Could not find Warehouse.</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
 
 export const Success = ({ warehouse }) => {
   return (
