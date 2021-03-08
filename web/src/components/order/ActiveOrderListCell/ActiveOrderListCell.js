6fb650966b32776aa4bab71b63e7f302b8de1731
Ryan <jesselockard99@gmail.com>
2021-03-08 01:04:18 -0500

    impl ActiveOrderListCell

--- web/src/components/order/ActiveOrderListCell/ActiveOrderListCell.js
+++ web/src/components/order/ActiveOrderListCell/ActiveOrderListCell.js
@@ -1,17 +1,32 @@
+import Card from 'react-bootstrap/Card'
+
+import LoadingCard from 'src/components/LoadingCard'
+import OrderList from 'src/components/order/OrderList'
+
 export const QUERY = gql`
-  query ActiveOrderListQuery {
-    activeOrderList {
+  query ACTIVE_ORDERS($warehouseId: String!) {
+    orders(warehouseId: $warehouseId) {
       id
+      orderNumber
+      jobName
+      updatedAt
+      createdAt
     }
   }
 `
 
-export const Loading = () => <div>Loading...</div>
+export const Loading = () => <LoadingCard />
 
-export const Empty = () => <div>Empty</div>
+export const Empty = () => (
+  <Card>
+    <Card.Body>
+      <Card.Title>No Active Orders Found</Card.Title>
+      <Card.Text>
+        Your Selected Warehouse does not have any Orders with Pallets tagged to
+        them.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+)
 
-export const Failure = ({ error }) => <div>Error: {error.message}</div>
-
-export const Success = ({ activeOrderList }) => {
-  return JSON.stringify(activeOrderList)
-}
+export const Success = ({ orders }) => <OrderList orders={orders} />
