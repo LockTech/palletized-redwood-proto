a44941be8db228bf88633fba3d1f6b6a353b8100
Ryan <jesselockard99@gmail.com>
2021-03-09 23:45:43 -0500

    impl OrderEditCell

--- web/src/components/order/OrderEditCell/OrderEditCell.js
+++ web/src/components/order/OrderEditCell/OrderEditCell.js
@@ -1,17 +1,76 @@
+import { useEffect } from 'react'
+import { navigate, routes } from '@redwoodjs/router'
+import { useFlash, useMutation } from '@redwoodjs/web'
+import Card from 'react-bootstrap/Card'
+
+import LoadingCard from 'src/components/LoadingCard'
+import OrderForm from 'src/components/order/OrderForm'
+
 export const QUERY = gql`
-  query OrderEditQuery {
-    orderEdit {
+  query FindOrderById($id: String!) {
+    order(id: $id) {
       id
+      orderNumber
+      jobName
     }
   }
 `
 
-export const Loading = () => <div>Loading...</div>
+const UPDATE_ORDER_MUTATION = gql`
+  mutation UpdateOrderMutation($id: String!, $input: UpdateOrderInput!) {
+    updateOrder(id: $id, input: $input) {
+      id
+      orderNumber
+      jobName
+    }
+  }
+`
 
-export const Empty = () => <div>Empty</div>
+export const Loading = () => <LoadingCard />
 
-export const Failure = ({ error }) => <div>Error: {error.message}</div>
+const EmptyFail = ({ id }) => {
+  const { addMessage } = useFlash()
 
-export const Success = ({ orderEdit }) => {
-  return JSON.stringify(orderEdit)
+  useEffect(() => {
+    addMessage(`Could not find Order: ${id}.`, {
+      variant: 'danger',
+    })
+  }, [addMessage, id])
+
+  return (
+    <Card>
+      <Card.Body>Could not find Order: {id}.</Card.Body>
+    </Card>
+  )
+}
+export const Empty = EmptyFail
+export const Failure = EmptyFail
+
+export const Success = ({ order }) => {
+  const { addMessage } = useFlash()
+  const [updateOrder, { loading, error }] = useMutation(UPDATE_ORDER_MUTATION, {
+    onCompleted: () => {
+      navigate(routes.orders())
+      addMessage('Order has been successfully updated.', {
+        variant: 'success',
+      })
+    },
+  })
+
+  const onSave = (input, id) => {
+    updateOrder({ variables: { id, input } })
+  }
+
+  return (
+    <Card>
+      <Card.Body>
+        <OrderForm
+          onSave={onSave}
+          resultError={error}
+          resultLoading={loading}
+          order={order}
+        />
+      </Card.Body>
+    </Card>
+  )
 }
