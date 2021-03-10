69804c9130724664872c1f0ff82a536c64b41e4e
Ryan <jesselockard99@gmail.com>
2021-03-09 22:01:09 -0500

    impl OrderDetails component

--- web/src/components/order/OrderDetails/OrderDetails.js
+++ web/src/components/order/OrderDetails/OrderDetails.js
@@ -1,9 +1,43 @@
-const OrderDetails = () => {
+import Table from 'react-bootstrap/Table'
+
+import LocaleTime from 'src/components/LocaleTime'
+import OrderDeleteModal from 'src/components/order/OrderDeleteModal'
+
+const OrderDetails = ({ order }) => {
   return (
-    <div>
-      <h2>{'OrderDetails'}</h2>
-      <p>{'Find me in ./web/src/components/OrderDetails/OrderDetails.js'}</p>
-    </div>
+    <>
+      <OrderDeleteModal />
+      <Table className="details-table">
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <td>{order.id}</td>
+          </tr>
+          <tr>
+            <th>Order Number</th>
+            <td>{order.orderNumber}</td>
+          </tr>
+          {order.jobName && (
+            <tr>
+              <th>Job Name</th>
+              <td>{order.jobName}</td>
+            </tr>
+          )}
+          <tr>
+            <th>Created</th>
+            <td>
+              <LocaleTime datetime={order.createdAt} />
+            </td>
+          </tr>
+          <tr>
+            <th>Last Updated</th>
+            <td>
+              <LocaleTime datetime={order.updatedAt} />
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   )
 }
 
