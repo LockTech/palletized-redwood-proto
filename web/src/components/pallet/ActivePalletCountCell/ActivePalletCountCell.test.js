b0c7b4ceb588dd2e8a3c893764f4db4746ef9502
Ryan <jesselockard99@gmail.com>
2021-03-09 22:26:34 -0500

    updated ActivePalletCountCell.test

--- web/src/components/pallet/ActivePalletCountCell/ActivePalletCountCell.test.js
+++ web/src/components/pallet/ActivePalletCountCell/ActivePalletCountCell.test.js
@@ -2,7 +2,7 @@ import { render } from '@redwoodjs/testing'
 import { Loading, Empty, Failure, Success } from './ActivePalletCountCell'
 import { standard } from './ActivePalletCountCell.mock'
 
-describe('ActivePalletCountCell', () => {
+describe('activePalletCountCell', () => {
   it('renders Loading successfully', () => {
     expect(() => {
       render(<Loading />)
@@ -10,25 +10,30 @@ describe('ActivePalletCountCell', () => {
   })
 
   it('renders Empty successfully', async () => {
-    expect(() => {
-      render(<Empty />)
-    }).not.toThrow()
+    expect(() => render(<Empty />)).not.toThrow()
+
+    const result = render(<Empty />)
+    expect(await result.findAllByText('0')).not.toBeUndefined()
   })
 
   it('renders Failure successfully', async () => {
+    const failureComp = <Failure error={new Error('Oh no')} />
+
     expect(() => {
-      render(<Failure error={new Error('Oh no')} />)
+      render(failureComp)
     }).not.toThrow()
+
+    const result = render(failureComp)
+    expect(await result.findAllByText('-')).not.toBeUndefined()
   })
 
-  // When you're ready to test the actual output of your component render
-  // you could test that, for example, certain text is present:
-  //
-  //   expect(screen.getByText('Hello, world')).toBeInTheDocument()
-
   it('renders Success successfully', async () => {
-    expect(() => {
-      render(<Success activePalletCount={standard().activePalletCount} />)
-    }).not.toThrow()
+    const val = standard().count
+    const successComp = <Success count={val} />
+
+    expect(() => successComp).not.toThrow()
+
+    const result = render(successComp)
+    expect(await result.findAllByText(val)).not.toBeUndefined()
   })
 })
