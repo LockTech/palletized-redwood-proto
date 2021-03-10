28d81dad44d0f27464eaa24b0c2f9bd38e80c255
Ryan <jesselockard99@gmail.com>
2021-03-09 23:46:17 -0500

    removed "delete" functionality from WarehouseDetails

--- web/src/components/warehouse/WarehouseDetails/WarehouseDetails.js
+++ web/src/components/warehouse/WarehouseDetails/WarehouseDetails.js
@@ -1,52 +1,10 @@
-import { useCallback, useState } from 'react'
-import { useMutation, useFlash } from '@redwoodjs/web'
-import { routes, navigate } from '@redwoodjs/router'
-import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
 
 import LocaleTime from 'src/components/LocaleTime'
-import WarehouseDeleteModal from 'src/components/warehouse/WarehouseDeleteModal'
-
-const DELETE_WAREHOUSE_MUTATION = gql`
-  mutation DeleteWarehouseMutation($id: String!) {
-    deleteWarehouse(id: $id) {
-      id
-    }
-  }
-`
 
 const WarehouseDetails = ({ warehouse }) => {
-  const { addMessage } = useFlash()
-  const [deleteWarehouseQuery] = useMutation(DELETE_WAREHOUSE_MUTATION, {
-    onCompleted: () => {
-      navigate(routes.warehouses())
-      addMessage('Warehouse has been successfully deleted.', {
-        variant: 'success',
-      })
-    },
-  })
-
-  const [deleteModalVis, setDeleteModalVis] = useState(false)
-  const onHideDeleteModal = useCallback(() => {
-    setDeleteModalVis(false)
-  }, [setDeleteModalVis])
-
-  const onDeleteClick = useCallback(() => {
-    setDeleteModalVis(true)
-  }, [setDeleteModalVis])
-  const onDeleteConfirm = useCallback(() => {
-    deleteWarehouseQuery({ variables: { id: warehouse.id } })
-    setDeleteModalVis(false)
-  }, [deleteWarehouseQuery, setDeleteModalVis, warehouse])
-
   return (
     <>
-      <WarehouseDeleteModal
-        name={warehouse.name}
-        onConfirm={onDeleteConfirm}
-        onHide={onHideDeleteModal}
-        show={deleteModalVis}
-      />
       <Table className="details-table">
         <tbody>
           <tr>
@@ -71,9 +29,6 @@ const WarehouseDetails = ({ warehouse }) => {
           </tr>
         </tbody>
       </Table>
-      <Button block onClick={onDeleteClick} variant="outline-danger">
-        Delete
-      </Button>
     </>
   )
 }
