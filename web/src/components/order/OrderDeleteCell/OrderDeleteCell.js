f2e9b3d069c44c4215189852aa474652ae18fec0
Ryan <jesselockard99@gmail.com>
2021-03-10 00:39:46 -0500

    impl OrderDeleteCell

---
+++ web/src/components/order/OrderDeleteCell/OrderDeleteCell.js
@@ -0,0 +1,72 @@
+import { useCallback, useEffect, useState } from 'react'
+import { navigate, routes } from '@redwoodjs/router'
+import { useFlash, useMutation } from '@redwoodjs/web'
+import Button from 'react-bootstrap/Button'
+
+import OrderDeleteModal from 'src/components/order/OrderDeleteModal'
+
+export const DELETE_ORDER_MUTATION = gql`
+  mutation DeleteOrderMutation($id: String!) {
+    deleteOrder(id: $id) {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const OrderDeleteCell = ({ id }) => {
+  const { addMessage } = useFlash()
+  const [deleteQuery, { called, error }] = useMutation(DELETE_ORDER_MUTATION, {
+    onCompleted: () => {
+      navigate(routes.orders())
+      addMessage('Order has been successfully deleted.', {
+        variant: 'success',
+      })
+    },
+  })
+
+  useEffect(() => {
+    if (called && error) {
+      addMessage(
+        <span>
+          <p>{error.name}</p>
+          <p>{error.message}</p>
+        </span>,
+        { variant: 'danger' }
+      )
+    }
+  }, [addMessage, called, error])
+
+  const [deleteModalVis, setDeleteModalVis] = useState(false)
+  const onHideDeleteModal = useCallback(() => {
+    setDeleteModalVis(false)
+  }, [setDeleteModalVis])
+
+  const onDeleteClick = useCallback(() => {
+    setDeleteModalVis(true)
+  }, [setDeleteModalVis])
+  const onDeleteConfirm = useCallback(() => {
+    deleteQuery({ variables: { id } })
+    setDeleteModalVis(false)
+  }, [deleteQuery, setDeleteModalVis, id])
+
+  return (
+    <>
+      <OrderDeleteModal
+        id={id}
+        onConfirm={onDeleteConfirm}
+        onHide={onHideDeleteModal}
+        show={deleteModalVis}
+      />
+      <Button block onClick={onDeleteClick} variant="outline-danger">
+        Delete
+      </Button>
+    </>
+  )
+}
+export default OrderDeleteCell
---
+++ web/src/components/order/OrderDeleteCell/OrderDeleteCell.stories.js
@@ -0,0 +1,20 @@
+import { Loading, Empty, Failure, Success } from './OrderDeleteCell'
+import { standard } from './OrderDeleteCell.mock'
+
+export const loading = () => {
+  return Loading ? <Loading /> : null
+}
+
+export const empty = () => {
+  return Empty ? <Empty /> : null
+}
+
+export const failure = () => {
+  return Failure ? <Failure error={new Error('Oh no')} /> : null
+}
+
+export const success = () => {
+  return Success ? <Success {...standard()} /> : null
+}
+
+export default { title: 'Cells/OrderDeleteCell' }
