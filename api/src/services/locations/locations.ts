ae16625c5d864ed1a9f7d180ee1c921dadcac79d
Ryan <jesselockard99@gmail.com>
2021-03-11 02:41:05 -0500

    impl Locations error-handling, createLocation

---
+++ api/src/services/locations/locations.ts
@@ -0,0 +1,74 @@
+import { UserInputError } from '@redwoodjs/api'
+import { PrismaError } from 'prisma-error-enum'
+import { db } from 'src/lib/db'
+import {
+  handleCommonErrors,
+  throwIfIsReserved,
+  throwIfTooLong,
+  throwUnexpectedError,
+} from 'src/lib/error'
+
+const MAX_NAME_LEN = 75
+
+const commonExceptions = (input: ILocation) => {
+  throwIfIsReserved(input.name, 'Location', 'Name')
+  throwIfTooLong(input.name, MAX_NAME_LEN, 'Location', 'Name')
+}
+
+const handleCommonOrderErrors = (error) => {
+  switch (error.code) {
+    case PrismaError.UniqueConstraintViolation: {
+      throw new UserInputError(
+        'One or more fields are not unique to your organization.',
+        {
+          messages: {
+            Name: [
+              'must be unique accross all other Locations at the selected Warehouse.',
+            ],
+          },
+        }
+      )
+    }
+
+    case PrismaError.RecordDoesNotExist: {
+      throw new UserInputError(`Could not find Location.`, {
+        messages: {
+          A: ['Location must exist to be updated.'],
+        },
+      })
+    }
+  }
+}
+
+// ==
+export const locations = () => {
+  return db.location.findMany()
+}
+//
+
+// ==
+export const createLocation = async ({ input }: { input: ILocation }) => {
+  commonExceptions(input)
+
+  input.id = input.name.replaceAll(' ', '-').toLowerCase()
+
+  try {
+    return await db.location.create({
+      data: input,
+    })
+  } catch (err) {
+    handleCommonErrors(err)
+    handleCommonOrderErrors(err)
+    throwUnexpectedError(err)
+  }
+}
+//
+
+// ==
+export const Location = {
+  warehouse: (_obj, { root }) =>
+    db.location.findUnique({ where: { id: root.id } }).warehouse(),
+  pallets: (_obj, { root }) =>
+    db.location.findUnique({ where: { id: root.id } }).pallets(),
+}
+//
