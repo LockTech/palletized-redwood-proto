21d3723908487f14ba36524032fd16eabb36fae7
Ryan <jesselockard99@gmail.com>
2021-03-11 06:57:59 -0500

    converted App to TS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,15 @@
 import { FatalErrorBoundary } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
-import Notifications from 'src/components/Notifications'
-import FatalErrorPage from 'src/pages/FatalErrorPage'
+import Notifications from 'src/components/Notifications/Notifications'
+import FatalErrorPage from 'src/pages/FatalErrorPage/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './index.scss'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodApolloProvider>
+    <RedwoodApolloProvider useAuth={() => null}>
       <Notifications />
       <Routes />
     </RedwoodApolloProvider>
