c427d5624ec892c89930c1f0df8cff4e0554c40a
Ryan <jesselockard99@gmail.com>
2021-03-11 17:46:54 -0500

    updated @types, removed FlashMessage

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,29 +1,22 @@
 declare interface IWarehouse {
   id: string
   name: string
-  createdAt: string
-  updatedAt: string
+  createdAt?: string
+  updatedAt?: string
 }
 
 declare interface ILocation {
   id: string
   name: string
   warehouse: IWarehouse
+  createdAt?: string
+  updatedAt?: string
 }
 
 declare interface IOrder {
   id: string
   orderNumber: string
   jobName?: string
-  createdAt: string
-  updatedAt: string
-}
-
-declare interface IFlashMessage {
-  id: number
-  skipTimeout?: boolean
-  text: string
-  timeoutTime?: number
-  title: string
-  variant: string
+  createdAt?: string
+  updatedAt?: string
 }
