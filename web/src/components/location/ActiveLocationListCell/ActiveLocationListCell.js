fb711bea6fda107b1e4686dc0c7c641ea00e6dcd
Ryan <jesselockard99@gmail.com>
2021-03-11 20:33:52 -0500

    impl ActiveLocationListCell

--- web/src/components/location/ActiveLocationListCell/ActiveLocationListCell.js
+++ web/src/components/location/ActiveLocationListCell/ActiveLocationListCell.js
@@ -1,17 +1,29 @@
+import {
+  Loading as LLoading,
+  Empty as LEmpty,
+  Failure as LFailure,
+  Success as LSuccess,
+} from 'src/components/location/LocationListCell'
+
 export const QUERY = gql`
-  query ActiveLocationListQuery {
-    activeLocationList {
+  query ACTIVE_LOCATIONS($warehouseId: String!) {
+    locations(warehouseId: $warehouseId) {
       id
+      name
+      updatedAt
+      createdAt
+      warehouse {
+        id
+        name
+      }
     }
   }
 `
 
-export const Loading = () => <div>Loading...</div>
+export const Loading = LLoading
 
-export const Empty = () => <div>Empty</div>
+export const Empty = LEmpty
 
-export const Failure = ({ error }) => <div>Error: {error.message}</div>
+export const Failure = LFailure
 
-export const Success = ({ activeLocationList }) => {
-  return JSON.stringify(activeLocationList)
-}
+export const Success = LSuccess
