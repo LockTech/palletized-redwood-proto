856f5b57e175de834d955eacb26067fcbb23e62d
Ryan <jesselockard99@gmail.com>
2021-03-13 03:53:02 -0500

    impl CreateProduct Dropdown item

--- web/src/components/product/ProductDropdown/ProductDropdown.tsx
+++ web/src/components/product/ProductDropdown/ProductDropdown.tsx
@@ -4,15 +4,26 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import Nav from 'react-bootstrap/Nav'
 
 const ProductDropdown: React.FC = () => {
+  const createProductRoute = useMemo(() => routes.createProduct(), [])
+  const createProductMatch = useMatch(createProductRoute).match
   const productsRoute = useMemo(() => routes.products(), [])
   const productsMatch = useMatch(productsRoute).match
 
   return (
     <Dropdown>
-      <Dropdown.Toggle as={Nav.Link} className={productsMatch && 'active'}>
+      <Dropdown.Toggle
+        as={Nav.Link}
+        className={(createProductMatch || productsMatch) && 'active'}
+      >
         Products
       </Dropdown.Toggle>
       <Dropdown.Menu>
+        <Dropdown.Item
+          className={createProductMatch && 'active'}
+          href={createProductRoute}
+        >
+          Create Product
+        </Dropdown.Item>
         <Dropdown.Item
           className={productsMatch && 'active'}
           href={productsRoute}
