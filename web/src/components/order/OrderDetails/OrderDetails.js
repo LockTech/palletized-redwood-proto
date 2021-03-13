6361b7743beac8291ea47634df6363625b9dc816
Ryan <jesselockard99@gmail.com>
2021-03-13 02:38:47 -0500

    refactored OrderDetaisl

--- web/src/components/order/OrderDetails/OrderDetails.js
+++ web/src/components/order/OrderDetails/OrderDetails.js
@@ -1,43 +1,39 @@
 import Table from 'react-bootstrap/Table'
 
 import LocaleTime from 'src/components/LocaleTime'
-import OrderDeleteModal from 'src/components/order/OrderDeleteModal'
 
 const OrderDetails = ({ order }) => {
   return (
-    <>
-      <OrderDeleteModal />
-      <Table className="details-table">
-        <tbody>
+    <Table className="details-table">
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <td>{order.id}</td>
+        </tr>
+        <tr>
+          <th>Order Number</th>
+          <td>{order.orderNumber}</td>
+        </tr>
+        {order.jobName && (
           <tr>
-            <th>ID</th>
-            <td>{order.id}</td>
+            <th>Job Name</th>
+            <td>{order.jobName}</td>
           </tr>
-          <tr>
-            <th>Order Number</th>
-            <td>{order.orderNumber}</td>
-          </tr>
-          {order.jobName && (
-            <tr>
-              <th>Job Name</th>
-              <td>{order.jobName}</td>
-            </tr>
-          )}
-          <tr>
-            <th>Created</th>
-            <td>
-              <LocaleTime datetime={order.createdAt} />
-            </td>
-          </tr>
-          <tr>
-            <th>Last Updated</th>
-            <td>
-              <LocaleTime datetime={order.updatedAt} />
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </>
+        )}
+        <tr>
+          <th>Created</th>
+          <td>
+            <LocaleTime datetime={order.createdAt} />
+          </td>
+        </tr>
+        <tr>
+          <th>Last Updated</th>
+          <td>
+            <LocaleTime datetime={order.updatedAt} />
+          </td>
+        </tr>
+      </tbody>
+    </Table>
   )
 }
 
