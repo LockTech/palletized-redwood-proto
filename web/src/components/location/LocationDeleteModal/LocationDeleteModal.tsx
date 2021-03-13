c5d6cbf96b266332bfa71e71051eccc45684f9e0
Ryan <jesselockard99@gmail.com>
2021-03-13 02:38:32 -0500

    impl LocationDeleteModal

--- web/src/components/location/LocationDeleteModal/LocationDeleteModal.tsx
+++ web/src/components/location/LocationDeleteModal/LocationDeleteModal.tsx
@@ -1,9 +1,38 @@
-const LocationDeleteModal: React.FunctionComponent = () => {
+import DeleteModal from 'src/components/DeleteModal/DeleteModal'
+import type { DeleteModalProps } from 'src/components/DeleteModal/DeleteModal'
+
+import LocationNameCell from 'src/components/location/LocationNameCell'
+import WarehouseNameCell from 'src/components/warehouse/WarehouseNameCell'
+
+export interface LocationDeleteModalProps extends DeleteModalProps {
+  id: string
+  warehouseId: string
+}
+
+const LocationDeleteModal: React.FC<LocationDeleteModalProps> = ({
+  id,
+  warehouseId,
+  ...otherProps
+}) => {
   return (
-    <div>
-      <h2>{'LocationDeleteModal'}</h2>
-      <p>{'Find me in ./web/src/components/LocationDeleteModal/LocationDeleteModal.tsx'}</p>
-    </div>
+    <DeleteModal {...otherProps}>
+      <p>
+        Are you sure you want to delete Location{' '}
+        <code>
+          <LocationNameCell id={id} />
+        </code>{' '}
+        from Warehouse{' '}
+        <code>
+          <WarehouseNameCell id={warehouseId} />
+        </code>
+        ?
+      </p>
+      <p>
+        <strong>
+          This action <u className="text-danger">cannot be undone</u>.
+        </strong>
+      </p>
+    </DeleteModal>
   )
 }
 
