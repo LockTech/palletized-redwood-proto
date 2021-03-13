8df00019e36e74a310713ebf6895a15977ecace8
Ryan <jesselockard99@gmail.com>
2021-03-13 18:02:24 -0500

    impl ActiveOrderTile, split ActiveOrderCountCell's concern

--- web/src/components/order/ActiveOrderCountCell/ActiveOrderCountCell.js
+++ web/src/components/order/ActiveOrderCountCell/ActiveOrderCountCell.js
@@ -1,9 +1,6 @@
 import { useEffect } from 'react'
-import { routes } from '@redwoodjs/router'
 import { toast } from '@redwoodjs/web/toast'
 import Skeleton from 'react-loading-skeleton'
-
-import TileCard from 'src/components/TileCard'
 
 export const QUERY = gql`
   query ActiveOrderCountQuery($id: String!) {
@@ -12,31 +9,9 @@ export const QUERY = gql`
   }
 `
 
-const CommonTileCard = ({ children }) => (
-  <TileCard
-    footer={{
-      text: 'Active Order list',
-      to: routes.orders({ active: true }),
-    }}
-    header="Active Orders"
-    headerTooltip={
-      <span>
-        Active Orders are Orders which have Pallets with the status
-        &quot;Not-Shipped&quot;.
-      </span>
-    }
-  >
-    <p className="mb-0 display-4">{children}</p>
-  </TileCard>
-)
+export const Loading = () => <Skeleton />
 
-export const Loading = () => (
-  <CommonTileCard>
-    <Skeleton />
-  </CommonTileCard>
-)
-
-export const Empty = () => <CommonTileCard>0</CommonTileCard>
+export const Empty = () => '0'
 
 export const Failure = ({ id }) => {
   useEffect(() => {
@@ -45,7 +20,7 @@ export const Failure = ({ id }) => {
     )
   }, [id])
 
-  return <CommonTileCard>-</CommonTileCard>
+  return '-'
 }
 
-export const Success = ({ count }) => <CommonTileCard>{count}</CommonTileCard>
+export const Success = ({ count }) => count
--- web/src/components/order/ActiveOrderTile/ActiveOrderTile.tsx
+++ web/src/components/order/ActiveOrderTile/ActiveOrderTile.tsx
@@ -1,9 +1,36 @@
-const ActiveOrderTile: React.FunctionComponent = () => {
+import { routes } from '@redwoodjs/router'
+
+import TileCard from 'src/components/TileCard/TileCard'
+import type { TileCardProps } from 'src/components/TileCard/TileCard'
+import ActiveOrderCountCell from 'src/components/order/ActiveOrderCountCell'
+
+export interface ActiveOrderTileProps extends TileCardProps {
+  warehouseId: string
+}
+
+const ActiveOrderTile: React.FC<ActiveOrderTileProps> = ({
+  warehouseId,
+  ...otherProps
+}) => {
   return (
-    <div>
-      <h2>{'ActiveOrderTile'}</h2>
-      <p>{'Find me in ./web/src/components/ActiveOrderTile/ActiveOrderTile.tsx'}</p>
-    </div>
+    <TileCard
+      footer={{
+        text: 'Active Order list',
+        to: routes.orders({ active: 'true' }),
+      }}
+      header="Active Orders"
+      headerTooltip={
+        <span>
+          Active Orders are Orders which have Pallets with the status
+          &quot;Not-Shipped&quot;.
+        </span>
+      }
+      {...otherProps}
+    >
+      <p className="mb-0 display-4">
+        <ActiveOrderCountCell id={warehouseId} />
+      </p>
+    </TileCard>
   )
 }
 
