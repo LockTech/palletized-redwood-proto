cf3087bd4c8b5b585aaa657e44ca400c9a0a4b43
Ryan <jesselockard99@gmail.com>
2021-03-13 18:03:03 -0500

    created UpcomingOrderTile

---
+++ web/src/components/order/UpcomingOrderTile/UpcomingOrderTile.stories.tsx
@@ -0,0 +1,7 @@
+import UpcomingOrderTile from './UpcomingOrderTile'
+
+export const generated = () => {
+  return <UpcomingOrderTile />
+}
+
+export default { title: 'Components/UpcomingOrderTile' }
---
+++ web/src/components/order/UpcomingOrderTile/UpcomingOrderTile.tsx
@@ -0,0 +1,10 @@
+const UpcomingOrderTile: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h2>{'UpcomingOrderTile'}</h2>
+      <p>{'Find me in ./web/src/components/UpcomingOrderTile/UpcomingOrderTile.tsx'}</p>
+    </div>
+  )
+}
+
+export default UpcomingOrderTile
