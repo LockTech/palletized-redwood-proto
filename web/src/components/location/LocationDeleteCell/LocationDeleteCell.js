b77e2bc8e92e6505206def349ed7e1357612d79c
Ryan <jesselockard99@gmail.com>
2021-03-13 02:38:27 -0500

    impl LocationDeleteCell

---
+++ web/src/components/location/LocationDeleteCell/LocationDeleteCell.js
@@ -0,0 +1,73 @@
+import { useCallback, useEffect, useState } from 'react'
+import { navigate, routes } from '@redwoodjs/router'
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
+import Button from 'react-bootstrap/Button'
+
+import LocationDeleteModal from 'src/components/location/LocationDeleteModal'
+import { QUERY } from 'src/components/location/LocationListCell'
+
+export const DELETE_LOCATION_MUTATION = gql`
+  mutation DeleteLocationMutation($id: String!) {
+    deleteLocation(id: $id) {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const LocationDeleteCell = ({ id, warehouseId }) => {
+  const [deleteQuery, { called, error }] = useMutation(
+    DELETE_LOCATION_MUTATION,
+    {
+      onCompleted: () => {
+        navigate(routes.locations())
+        toast.success('Location has been successfully deleted.')
+      },
+      refetchQueries: [{ query: QUERY }],
+      awaitRefetchQueries: true,
+    }
+  )
+
+  useEffect(() => {
+    if (called && error) {
+      toast.error(error.message)
+    }
+  }, [called, error])
+
+  const [deleteModalVis, setDeleteModalVis] = useState(false)
+
+  const onHideDeleteModal = useCallback(() => {
+    setDeleteModalVis(false)
+  }, [setDeleteModalVis])
+
+  const onDeleteClick = useCallback(() => {
+    setDeleteModalVis(true)
+  }, [setDeleteModalVis])
+
+  const onDeleteConfirm = useCallback(() => {
+    deleteQuery({ variables: { id } })
+    setDeleteModalVis(false)
+  }, [deleteQuery, setDeleteModalVis, id])
+
+  return (
+    <>
+      <LocationDeleteModal
+        id={id}
+        onConfirm={onDeleteConfirm}
+        onHide={onHideDeleteModal}
+        show={deleteModalVis}
+        warehouseId={warehouseId}
+      />
+      <Button block onClick={onDeleteClick} variant="outline-danger">
+        Delete
+      </Button>
+    </>
+  )
+}
+export default LocationDeleteCell
