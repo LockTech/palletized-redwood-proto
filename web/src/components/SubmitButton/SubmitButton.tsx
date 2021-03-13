816a1683f082480987306f4c1413afc930d4623c
Ryan <jesselockard99@gmail.com>
2021-03-13 04:19:29 -0500

    created SubmitButton component

---
+++ web/src/components/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,10 @@
+const SubmitButton: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h2>{'SubmitButton'}</h2>
+      <p>{'Find me in ./web/src/components/SubmitButton/SubmitButton.tsx'}</p>
+    </div>
+  )
+}
+
+export default SubmitButton
