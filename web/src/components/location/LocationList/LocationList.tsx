c83946fa2b57e4b7314f86b70944910ec106c1ae
Ryan <jesselockard99@gmail.com>
2021-03-14 01:25:57 -0500

    added Order and Pallet count to LocationList details

--- web/src/components/location/LocationList/LocationList.tsx
+++ web/src/components/location/LocationList/LocationList.tsx
@@ -2,10 +2,16 @@ import Card from 'react-bootstrap/Card'
 import CardColumns from 'react-bootstrap/CardColumns'
 
 import LocationDeleteCell from 'src/components/location/LocationDeleteCell'
+import OrderCountCell from 'src/components/order/OrderCountCell'
+import PalletCountCell from 'src/components/pallet/PalletCountCell'
 
 export interface LocationListProps {
   locations: ILocation[]
 }
+
+const countContainerClasses =
+  'd-flex flex-direction-row justify-content-between'
+const countCountClasses = 'text-monospace'
 
 const LocationList: React.FC<LocationListProps> = ({ locations }) => {
   return (
@@ -17,6 +23,18 @@ const LocationList: React.FC<LocationListProps> = ({ locations }) => {
             <Card.Subtitle className="mb-3 text-muted">
               {loc.warehouse.name}
             </Card.Subtitle>
+            <Card.Text className={countContainerClasses}>
+              <strong>Order Count:</strong>
+              <span className={countCountClasses}>
+                <OrderCountCell locationId={loc.id} />
+              </span>
+            </Card.Text>
+            <Card.Text className={countContainerClasses}>
+              <strong>Pallet Count:</strong>
+              <span className={countCountClasses}>
+                <PalletCountCell pallet={{ locationId: loc.id }} />
+              </span>
+            </Card.Text>
             <LocationDeleteCell id={loc.id} warehouseId={loc.warehouse.id} />
           </Card.Body>
         </Card>
