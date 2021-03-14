1dbff3115203ec7fe340fdd890d5048041037a0b
Ryan <jesselockard99@gmail.com>
2021-03-14 18:27:05 -0400

    added Recoil Provider to App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,24 @@
 import { FatalErrorBoundary } from '@redwoodjs/web'
 import { Toaster } from '@redwoodjs/web/toast'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import { RecoilRoot } from '@salvoravida/recoil'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './index.scss'
 
-const App = () => (
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodApolloProvider>
-      <Toaster />
-      <Routes />
-    </RedwoodApolloProvider>
-  </FatalErrorBoundary>
-)
+const App = () => {
+  return (
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodApolloProvider>
+        <RecoilRoot>
+          <Toaster />
+          <Routes />
+        </RecoilRoot>
+      </RedwoodApolloProvider>
+    </FatalErrorBoundary>
+  )
+}
 
 export default App
