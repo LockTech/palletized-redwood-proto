925d9a388f336ac36bd60dfc4e29e9200f289dd3
Ryan <jesselockard99@gmail.com>
2021-03-14 01:26:08 -0500

    updated literal-string on LocationListPage

--- web/src/pages/LocationListPage/LocationListPage.js
+++ web/src/pages/LocationListPage/LocationListPage.js
@@ -23,7 +23,7 @@ const LocationListPage = ({ active = true }) => {
           <Col>
             <h1>Locations</h1>
             <p className="text-muted">
-              A list of the Locations accessible by your organization.
+              A list of all the Locations accessible by your organization.
             </p>
           </Col>
         </Row>
