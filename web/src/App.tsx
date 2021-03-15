59f2ff01bb3d797e192166d1b1345188be1867c7
Ryan <jesselockard99@gmail.com>
2021-03-14 23:49:46 -0400

    impl AtomProvider on App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,12 +7,14 @@ import FatalErrorPage from 'src/pages/FatalErrorPage/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './index.scss'
+import AtomProvider from './components/AtomProvider/AtomProvider'
 
 const App = () => {
   return (
     <FatalErrorBoundary page={FatalErrorPage}>
       <RedwoodApolloProvider>
         <RecoilRoot>
+          <AtomProvider />
           <Toaster />
           <Routes />
         </RecoilRoot>
