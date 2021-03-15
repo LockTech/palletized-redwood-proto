1230ac3b78ebd6d435e8ca479b8239299552b0be
Ryan <jesselockard99@gmail.com>
2021-03-15 02:44:47 -0400

    updated DefaultWarehouseAtom

--- web/src/atoms/DefaultWarehouseAtom/DefaultWarehouseAtom.ts
+++ web/src/atoms/DefaultWarehouseAtom/DefaultWarehouseAtom.ts
@@ -1,16 +1,8 @@
 import { atom } from '@salvoravida/recoil'
 
-export type DefaultWarehouseType = {
-  id: string
-  name: string
-}
-
-const DefaultWarehouseAtom = atom<DefaultWarehouseType>({
+const DefaultWarehouseAtom = atom<IUserDefaultWarehouse>({
   key: 'defaultWarehouseAtom',
-  default: {
-    id: 'charleston',
-    name: 'Charleston',
-  }, // '' in prod-loaded from server or localStorage
+  default: null, // '' in prod-loaded from server or localStorage
 })
 
 export default DefaultWarehouseAtom
