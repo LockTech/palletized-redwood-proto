ea1bdc4441bba280c17f206bef4515a07100b76f
Ryan <jesselockard99@gmail.com>
2021-03-17 22:05:35 -0400

    added resourceName override to SwitchWarehouse component

--- web/src/components/warehouse/SwitchWarehouseForm/SwitchWarehouseForm.tsx
+++ web/src/components/warehouse/SwitchWarehouseForm/SwitchWarehouseForm.tsx
@@ -7,12 +7,22 @@ export interface SwitchWarehouseFormProps {
   isActive: boolean
   onClick: (isActive: boolean) => void
   onToggleActive: (isActive: boolean) => void
+  /**
+   * Provide further clarification as to which *resource* is being filtered.
+   */
+  resourceName?: string
 }
 
+/**
+ * Utility component for providing easy-to-configure "Include only 'resource' which belong to their Default Warehouse" detection.
+ *
+ * When `isActive` is set to `true`, a `<Button>` will be included to prompt the user to switch their Warehouse.
+ */
 const SwitchWarehouseForm: React.FC<SwitchWarehouseFormProps> = ({
   isActive,
   onClick,
   onToggleActive,
+  resourceName,
 }) => {
   const { register, watch } = useForm({
     mode: 'onChange',
@@ -35,8 +45,9 @@ const SwitchWarehouseForm: React.FC<SwitchWarehouseFormProps> = ({
         className={watchDefaultWarehouses && 'mb-3'}
         id="defaultWarehouses"
         label={
-          <span>
-            Only include <em>resources</em> found at your Selected Warehouse.
+          <span className="user-select-none">
+            Only include <em>{resourceName || 'resources'}</em> found at your
+            Default Warehouse.
           </span>
         }
         name="defaultWarehouses"
