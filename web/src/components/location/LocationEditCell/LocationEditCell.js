1fc9a0cff3ba2bf60042f52d72747cb83d9822a3
Ryan <jesselockard99@gmail.com>
2021-03-19 04:41:55 -0400

    impl LocationEditCell

---
+++ web/src/components/location/LocationEditCell/LocationEditCell.js
@@ -0,0 +1,107 @@
+import { useEffect } from 'react'
+import { navigate, routes } from '@redwoodjs/router'
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
+import Card from 'react-bootstrap/Card'
+
+import LoadingCard from 'src/components/LoadingCard'
+import LocationForm from 'src/components/location/LocationForm/LocationForm'
+import LocationNameCell from 'src/components/location/LocationNameCell/LocationNameCell'
+import LocationTooltip from 'src/components/location/LocationTooltip/LocationTooltip'
+
+export const QUERY = gql`
+  query FindLocationById($id: String!) {
+    location(id: $id) {
+      id
+      name
+      warehouse {
+        id
+        name
+      }
+    }
+  }
+`
+
+const UPDATE_LOCATION_MUTATION = gql`
+  mutation UpdateLocationMutation($id: String!, $input: UpdateLocationInput!) {
+    updateLocation(id: $id, input: $input) {
+      id
+      name
+      warehouse {
+        id
+        name
+      }
+    }
+  }
+`
+
+const commonHeader = (id) => (
+  <Card.Header className="d-flex flex-direction-row align-items-center justify-content-between">
+    <span>
+      Editing Location:&nbsp;
+      <strong>
+        <LocationNameCell id={id} />
+      </strong>
+    </span>
+    <LocationTooltip />
+  </Card.Header>
+)
+
+export const Loading = () => <LoadingCard />
+
+export const Empty = ({ id }) => {
+  useEffect(() => {
+    toast.error(`Could not find Location: ${id}.`)
+  }, [id])
+
+  return (
+    <Card>
+      {commonHeader(id)}
+      <Card.Body>Could not find Location: {id}.</Card.Body>
+    </Card>
+  )
+}
+
+export const Failure = ({ id }) => {
+  useEffect(() => {
+    toast.error(`An error occured while trying to edit Location: ${id}.`)
+  }, [id])
+
+  return (
+    <Card>
+      {commonHeader(id)}
+      <Card.Body>An error occured while trying to edit the Location.</Card.Body>
+    </Card>
+  )
+}
+
+export const Success = ({ location }) => {
+  const [updateLocation, { loading, error }] = useMutation(
+    UPDATE_LOCATION_MUTATION,
+    {
+      onCompleted: () => {
+        navigate(routes.locations())
+        toast.success('Location has been successfully updated.')
+      },
+    }
+  )
+
+  const onSave = (input, id) => {
+    console.log(input)
+    updateLocation({ variables: { id, input } })
+  }
+
+  return (
+    <Card>
+      {commonHeader(location.id)}
+      <Card.Body>
+        <LocationForm
+          onSave={onSave}
+          error={error}
+          loading={loading}
+          location={location}
+        />
+      </Card.Body>
+    </Card>
+  )
+}
