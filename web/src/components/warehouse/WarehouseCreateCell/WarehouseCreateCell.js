1cf97f60b43ae99f2ff03b73fb1fce02b69c0edc
Ryan <jesselockard99@gmail.com>
2021-03-19 20:05:42 -0400

    added Header to WarehouseCreate Card

--- web/src/components/warehouse/WarehouseCreateCell/WarehouseCreateCell.js
+++ web/src/components/warehouse/WarehouseCreateCell/WarehouseCreateCell.js
@@ -3,7 +3,8 @@ import { toast } from '@redwoodjs/web/toast'
 import { navigate, routes } from '@redwoodjs/router'
 import Card from 'react-bootstrap/Card'
 
-import WarehouseForm from 'src/components/warehouse/WarehouseForm'
+import WarehouseForm from 'src/components/warehouse/WarehouseForm/WarehouseForm'
+import WarehouseTooltip from 'src/components/warehouse/WarehouseTooltip/WarehouseTooltip'
 
 const WAREHOUSE_CREATE_MUTATION = gql`
   mutation WarehouseCreateMutation($input: CreateWarehouseInput!) {
@@ -34,12 +35,12 @@ const WarehouseCreateCell = () => {
 
   return (
     <Card>
+      <Card.Header className="d-flex flex-direction-row align-items-center justify-content-between">
+        New Warehouse
+        <WarehouseTooltip />
+      </Card.Header>
       <Card.Body>
-        <WarehouseForm
-          onSave={onSave}
-          error={error}
-          loading={loading}
-        />
+        <WarehouseForm onSave={onSave} error={error} loading={loading} />
       </Card.Body>
     </Card>
   )
