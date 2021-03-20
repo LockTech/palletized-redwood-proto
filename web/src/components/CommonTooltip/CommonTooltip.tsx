756da5d7384cc47107170f99517cfadf3a91f0ee
Ryan <jesselockard99@gmail.com>
2021-03-19 20:20:05 -0400

    updated props for CommonTooltip

--- web/src/components/CommonTooltip/CommonTooltip.tsx
+++ web/src/components/CommonTooltip/CommonTooltip.tsx
@@ -1,16 +1,37 @@
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import type { OverlayTriggerProps } from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
+import type { TooltipProps } from 'react-bootstrap/Tooltip'
 import { BsInfoCircle } from 'react-icons/bs'
+import type { IconBaseProps } from 'react-icons'
 
-const CommonTooltip: React.FunctionComponent = ({ children }) => {
+export interface CommonTooltipProps {
+  iconProps: Partial<IconBaseProps>
+  overlayProps: Partial<OverlayTriggerProps>
+  tooltipProps: Partial<TooltipProps>
+}
+
+const CommonTooltip: React.FC<CommonTooltipProps> = ({
+  children,
+  iconProps,
+  overlayProps,
+  tooltipProps,
+  ...otherProps
+}) => {
   return (
     <OverlayTrigger
       trigger={['click', 'hover', 'focus']}
       placement="bottom-end"
       delay={{ show: 30, hide: 250 }}
-      overlay={<Tooltip id="locationTooltip">{children}</Tooltip>}
+      overlay={
+        <Tooltip id="locationTooltip" {...tooltipProps}>
+          {children}
+        </Tooltip>
+      }
+      {...overlayProps}
+      {...otherProps}
     >
-      <BsInfoCircle className="ml-2" />
+      <BsInfoCircle className="ml-2" {...iconProps} />
     </OverlayTrigger>
   )
 }
