1cce1656bb2d35dbfe47e21985c3f09fc79e39aa
Ryan <jesselockard99@gmail.com>
2021-03-19 20:21:28 -0400

    added Header to ProductCreateCell Card

--- web/src/components/product/ProductCreateCell/ProductCreateCell.js
+++ web/src/components/product/ProductCreateCell/ProductCreateCell.js
@@ -3,7 +3,8 @@ import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 import Card from 'react-bootstrap/Card'
 
-import ProductForm from 'src/components/product/ProductForm'
+import ProductForm from 'src/components/product/ProductForm/ProductForm'
+import ProductTooltip from 'src/components/product/ProductTooltip/ProductTooltip'
 
 export const PRODUCT_CREATE_MUTATION = gql`
   mutation ProductCreateMutation($input: CreateProductInput!) {
@@ -30,6 +31,10 @@ const ProductCreateCell = () => {
 
   return (
     <Card>
+      <Card.Header className="d-flex flex-direction-row justify-content-between">
+        New Product
+        <ProductTooltip />
+      </Card.Header>
       <Card.Body>
         <ProductForm onSave={onSave} error={error} loading={loading} />
       </Card.Body>
