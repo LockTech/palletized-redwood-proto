ace5ebfe5d46784c37cd27dcb140122d952be440
Ryan <jesselockard99@gmail.com>
2021-03-23 04:00:16 -0400

    removed Alert from WHEditPage

--- web/src/pages/WHEditPage/WHEditPage.js
+++ web/src/pages/WHEditPage/WHEditPage.js
@@ -1,11 +1,9 @@
-import Alert from 'react-bootstrap/Alert'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
 import DashLayout from 'src/layouts/DashLayout/DashLayout'
 import WarehouseEditCell from 'src/components/warehouse/WarehouseEditCell'
-import WarehouseNameCell from 'src/components/warehouse/WarehouseNameCell'
 
 const WHEditPage = ({ id }) => {
   return (
@@ -17,17 +15,6 @@ const WHEditPage = ({ id }) => {
             <p className="text-muted">
               Alter the details of a Warehouse managed by your organization.
             </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Alert variant="info">
-              You are currently editing the{' '}
-              <strong>
-                <WarehouseNameCell id={id} />
-              </strong>{' '}
-              Warehouse.
-            </Alert>
           </Col>
         </Row>
         <Row>
