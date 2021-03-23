37c56bf81f1495d6abf3786c9d97dd76031b5d14
Ryan <jesselockard99@gmail.com>
2021-03-23 19:08:14 -0400

    created IPallet interface

--- typings/Pallet.d.ts
+++ typings/Pallet.d.ts
@@ -1,1 +1,12 @@
-declare interface IPallet {}
+declare type TPalletStatus = 'ACTIVE' | 'SHIPPED'
+
+declare interface IPallet {
+  id: string
+  name: string
+  createdAt?: string
+  updatedAt?: string
+  location?: ILocation
+  order?: IOrder
+  product: IProduct[]
+  status: TPalletStatus
+}
