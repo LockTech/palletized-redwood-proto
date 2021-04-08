d782ce559115b05b8c4eacb01e8f5aa210a7a157
Ryan <jesselockard99@gmail.com>
2021-04-07 22:37:02 -0400

    created PalletCountCell?

---
+++ web/src/components/pallet/PalletCountCell/PalletCountCell.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import { toast } from '@redwoodjs/web/toast'
+import Skeleton from 'react-loading-skeleton'
+
+export const QUERY = gql`
+  query ActivePalletCountQuery($pallet: QueryPalletInput) {
+    count: countPallets(id: $pallet)
+  }
+`
+
+export const Loading = () => <Skeleton />
+
+export const Empty = () => '0'
+
+export const Failure = ({ id }) => {
+  useEffect(() => {
+    toast.error(
+      `An error occured while retrieving the Active-Pallet count for Order: ${id}`
+    )
+  }, [id])
+
+  return '-'
+}
+
+export const Success = ({ count }) => count
