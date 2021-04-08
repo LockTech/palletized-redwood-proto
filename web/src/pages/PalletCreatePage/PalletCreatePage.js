aa2613b5c581e06c432725a79c0061268f213da0
Ryan <jesselockard99@gmail.com>
2021-04-07 22:37:18 -0400

    impl PalletCreateCell on PalletCreatePage

--- web/src/pages/PalletCreatePage/PalletCreatePage.js
+++ web/src/pages/PalletCreatePage/PalletCreatePage.js
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/Row'
 
 import DashLayout from 'src/layouts/DashLayout/DashLayout'
 
+import PalletCreateCell from 'src/components/pallet/PalletCreateCell/PalletCreateCell'
+
 const PalletCreatePage = () => {
   return (
     <DashLayout>
@@ -11,6 +13,15 @@ const PalletCreatePage = () => {
         <Row>
           <Col>
             <h1>Create Pallet</h1>
+            {/* <p className="text-muted">
+              Create a new Pallet, tagging it to the selected
+              Warehouse-and-Location, with the Products you specify.
+            </p> */}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <PalletCreateCell />
           </Col>
         </Row>
       </Container>
